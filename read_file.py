d88b4785ff2f28878ae46058c2c7a81ac0c12e7c
Muhaammad Ilhaam Ghiffari <126216608+IlhaamGhiffari@users.noreply.github.com>
2024-09-19 10:13:01 +0000

    .

--- read_file.py
+++
@@ -1,9 +0,0 @@
-from hdfs import InsecureClient
-
-# Connect to HDFS Namenode
-client = InsecureClient('http://localhost:9870', user='root')
-
-# Reading a file from HDFS
-with client.read('/hdfs/path/file.txt') as reader:
-    data = reader.read()
-    print(data)
